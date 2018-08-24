59fc119f317eb73626ae94df36198dd22dd18f9d
Ashwini <30803844+ashwinibit@users.noreply.github.com>
2018-08-25 01:38:17 +0530

    Update subadmin.php

--- subadmin/subadmin.php
+++ subadmin/subadmin.php
@@ -6,12 +6,59 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="main.js"></script>
+	
+	
+	<style>
+	body{
+		background-color:;
+		margin:0;
+		padding:0;
+	}
+	.card{
+	 margin-left:200px;
+	  margin-right:200px;
+	  margin-top:30px;
+	  padding:40px;
+	}
+	.banner img{
+	width:400px;
+	height:400px;
+	margin-top:50px;
+	
+	}
+	
+	</style>
 </head>
 <body>
+
+
        <?php
 include ('../nav/navsubadmin.php');
 
 ?> 
+
+
+
+<div class='card bg-primary'>
+ <h3 align='center' class='text-light'>ANJOR</h3>
+</div>
+
+<div class='container banner col-12 row'>
+
+<img src='../img/Tea_leaf.png'>
+<div class='container'>
+<h4>
+Welcome to Anjor
+</h4>
+
+
+
+</div>
+
+
+</div>
+
 </body>
 </html>
